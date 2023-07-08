ed0069189387ef89474f26e3db6f68d72a1d9e12
DanielDomingueti <danieldomingueti@hotmail.com>
2023-07-08 15:28:10 -0300

    feat: ex 14 and ex15

--- exercicios_aula_12/ex14.c
+++ exercicios_aula_12/ex14.c
@@ -7,9 +7,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int* sumArrays(int* a, int* b, int size);
+
+int* sumArrays(int* a, int* b, int size) {
+    int i;
+    int* soma = NULL;
+
+    soma = (int*) malloc(size * sizeof(int));
+
+    if (!soma) {
+        printf("erro de alocacao");
+        exit(1);
+    }
+    
+    int* sumPointer = soma;
+    int* aPointer = a;
+    int* bPointer = b;
+
+
+        
+    for (i = 0; i < size; i++) {
+        *sumPointer = *aPointer + *bPointer;
+        //printf("\nValor de A: %d, B: %d, Soma: %d\n", *aPointer, *bPointer, *sumPointer);
+        sumPointer++;
+        aPointer++;
+        bPointer++;
+    }
+    
+    return soma;
+}
+
 int main() {
 
-    
+    int size, i, input;
+    int* a = NULL;
+    int* b = NULL;
+
+    scanf(" %d", &size);
+
+    a = (int*) malloc(size * sizeof(int));
+    b = (int*) malloc(size * sizeof(int));
+
+    //leitura a
+    for (i = 0; i < size; i++) {
+        //printf("Leitura do vetor A posicao %d: ", i+1);
+        scanf(" %d", &input);
+        a[i] = input;
+    }
+
+    //leitura b
+    for (i = 0; i < size; i++) {
+        //printf("Leitura do vetor B posicao %d: ", i+1);
+        scanf(" %d", &input);
+        b[i] = input;
+    }
+
+    int* c = NULL;
+    c = sumArrays(a, b, size);
+
+    for (i = 0; i < size; i++) {
+        printf("%d ", c[i]);
+    }
 
     return 0;
 }
--- exercicios_aula_12/ex15.c
+++ exercicios_aula_12/ex15.c
@@ -6,9 +6,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void findSmallestValue(int* arr, int size, int* min, int* max);
+
+void findSmallestValue(int* arr, int size, int* min, int* max) {
+    int i;
+    int* initialValueArr = arr;
+    int minValue = initialValueArr[0];
+    int maxValue = initialValueArr[0];
+
+    for (i = 1; i < size; i++) {
+        initialValueArr++;
+        if (*initialValueArr > maxValue) {
+            maxValue = *initialValueArr;
+        }
+        if (*initialValueArr < minValue) {
+            minValue = *initialValueArr;
+        }
+    }
+    *min = minValue;
+    *max = maxValue;
+}
+
 int main() {
 
-    
+    int i, size, input;
+    int* arr = NULL;
+    int* min = NULL;
+    int* max = NULL;
+
+    scanf(" %d", &size);
+
+    arr = (int*) malloc(size * sizeof(int));
+    min = (int*) malloc(sizeof(int));
+    max = (int*) malloc(sizeof(int));
+
+    if (!arr || !min || !max) {
+        printf("falha na alocacao\n");
+        exit(1);
+    }
+
+    for (i = 0; i < size; i++) {
+        scanf(" %d", &input);
+        arr[i] = input;
+    }
+
+    findSmallestValue(arr, size, min, max);
+
+    printf("%d %d", *min, *max);
+  
+    free(arr);
+    free(min);
+    free(max);
 
     return 0;
 }
